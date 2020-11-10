0fd83bd1d683c6982e477176701758375f18ac5b
Beka <bektemir.abdymomunov@mail.ru>
2020-11-10 17:50:14 -0500

    project is already in remote repo , committing more changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //more stuff
         // Mac user and windows users are happy to see commit dialog finally
+
+        // I have already published on Github.com
+        // now i am ready to make more commits
+        // but do i need to share again  ????
+        // Absolutely no
+        // IT'S TIME COMMIT AND JUST PUSH
         System.out.println("Hello");
     }
 }
