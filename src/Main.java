5b3cc85104699d5afcc5c8b3bbd2382d89bc7b4f
Beka <bektemir.abdymomunov@mail.ru>
2020-11-10 17:43:22 -0500

    fixed mac and windows commit dialog issue

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //more stuff
+        // Mac user and windows users are happy to see commit dialog finally
         System.out.println("Hello");
     }
 }
