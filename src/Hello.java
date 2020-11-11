bdc6da581178f7999df96d6c6508483868dfc5c8
Beka <bektemir.abdymomunov@mail.ru>
2020-11-10 19:23:18 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    // more stuff
+    // Mac user and windows users are happy to see commit dialog finally
+
+    // I have already published on Github.com
+    // now i am ready to make more commits
+    // but do i need to share again  ????
+    // Absolutely no
+    // IT'S TIME COMMIT AND JUST PUSH
+}
